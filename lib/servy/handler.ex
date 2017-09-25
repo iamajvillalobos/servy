c1a521cd5c60f95f10c0f658ba687c2f31ce1a06
Arman Jon Villalobos <me@armanjon.com>
2017-09-25 22:11:16 +0800

    Create another route function that pattern matches on different route

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,9 +2,12 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
+
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     [method, path, _version] = 
@@ -15,8 +18,16 @@ defmodule Servy.Handler do
     %{ method: method, path: path, resp_body: "" }
   end
 
-  def route(conv) do
-    %{ conv | resp_body: "Bear, Lions, Tigers" }
+  # def route(conv) do
+  #   route(conv, conv.method, conv.path)
+  # end
+
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
+    %{ conv | resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
   end
 
   def format_response(conv) do
@@ -37,3 +48,22 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
+IO.puts Servy.Handler.handle(request)
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+IO.puts Servy.Handler.handle(request)
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+IO.puts Servy.Handler.handle(request)
