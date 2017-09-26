4e80439fa240e977bbb97df28febef4d4b307396
Arman Jon Villalobos <me@armanjon.com>
2017-09-26 19:40:22 +0800

    Add status code and reason

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -15,29 +15,40 @@ defmodule Servy.Handler do
       |> String.split("\n")
       |> List.first
       |> String.split(" ")
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method, path: path, resp_body: "", status: nil }
   end
 
-  # def route(conv) do
-  #   route(conv, conv.method, conv.path)
-  # end
-
   def route(%{method: "GET", path: "/wildthings"} = conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | resp_body: "Bears, Lions, Tigers", status: 200 }
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+    %{ conv | resp_body: "Teddy, Smokey, Paddington", status: 200 }
+  end
+
+  def route(%{method: "GET", path: "/bears" <> id} = conv) do
+    %{ conv | resp_body: "Bears, Lions, Tigers", status: 200 }
+  end
+
+  def route(conv) do
+    %{ conv | resp_body: "No #{conv.path} here!", status: 404 }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{conv.resp_body |> byte_size}
   
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      404 => "Not Found"
+    }[code]
   end
 end
 
@@ -61,6 +72,15 @@ IO.puts Servy.Handler.handle(request)
 
 request = """
 GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+IO.puts Servy.Handler.handle(request)
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
